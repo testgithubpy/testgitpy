2a814473279c0b21cd24eade4aee1bba26014fce
testgithubpy <sailajanavn@gmail.com>
2024-06-12 06:45:33 +0530

    c1

---
+++ lab004.py
@@ -0,0 +1,11 @@
+#format string
+print(2*1)
+num=90
+print(f"The number is {num*2}")
+b=1
+print(f"{b}*1={b}")
+print("2*{}={},{}". format(b,b*2,b*3))
+
+num=5
+print(f"{num}*1={num}")
+print(f"{num*2}={num*2}")
